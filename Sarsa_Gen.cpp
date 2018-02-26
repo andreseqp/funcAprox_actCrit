f1df0dbebaaba16d0c82985f683d32bc0ffde76d
QUINONES PARADES Andrès <andres.quinones@unine.ch>
2018-02-26 16:35:35 +0100

    update to 3.1 version of nlohmann/json (still unsuccesful)

--- Sarsa_Gen.cpp
+++ Sarsa_Gen.cpp
@@ -43,11 +43,15 @@ Last edit date:
 #include <iostream>
 #include <fstream>
 // Random number generator
-#include "M:\\Routines\\C++\\RandomNumbers\\random.h" //H for house pc, E for laptop, M for Office
+#include "M:\\Routines\\C++\\RandomNumbers\\random.h" 
+//H for house pc, E for laptop, M for Office
 #include "M:\\Routines\\C++\\RandomNumbers\\stdafx.h"
 // Classes
 #include "cleaner.h"
 #include "Client.h"
+// Jason parser
+#include "D:\\quinonesa\\Dropbox\C++\\json.hpp"       
+// Header for reading and using JSON files see https://github.com/nlohmann/json
 
 #define GET_VARIABLE_NAME(Variable) (#Variable)
 
@@ -68,9 +72,18 @@ void draw(client trainingSet[], int rounds, double &probRes, double &probVis)
 	for (int i = 0; i < rounds * 2; i++)
 	{
 		rndNum = rnd::uniform();
-		if (rndNum < cumProbs[0]) { trainingSet[i] = client(resident,residMeans,residSds,mins,residProbs,ResReward); }
-		else if (rndNum < cumProbs[1]) { trainingSet[i] = client(visitor, visitMeans, visitSds, mins, visitProbs, VisReward); }
-		else { trainingSet[i] = client(absence,residMeans,residSds,mins,residProbs,ResReward); }
+		if (rndNum < cumProbs[0]) 
+		{ 
+			trainingSet[i] = client(resident,residMeans,residSds,mins,residProbs,ResReward); 
+		}
+		else if (rndNum < cumProbs[1]) 
+		{ 
+			trainingSet[i] = client(visitor, visitMeans, visitSds, mins, visitProbs, VisReward); 
+		}
+		else 
+		{ 
+			trainingSet[i] = client(absence,residMeans,residSds,mins,residProbs,ResReward); 
+		}
 	}
 }
 
@@ -107,51 +120,106 @@ string create_filename(std::string filename, agent &individual, int &seed, doubl
 
 void initializeIndFile(ofstream &indOutput, agent &learner, int &seed,double &bias)
 {
-	std::string namedir = "C:\\Users\\quinonesa\\prelimResults\\functionAprox\\";// "H:\\Dropbox\\Neuchatel\\prelimResults\\functionAprox\\"; // "E:\\Dropbox\\Neuchatel\\prelimResults\\Set_15\\IndTrain_equVal"
+	std::string namedir = "C:\\Users\\quinonesa\\prelimResults\\functionAprox\\";
+	// "H:\\Dropbox\\Neuchatel\\prelimResults\\functionAprox\\"; // "E:\\Dropbox\\Neuchatel\\prelimResults\\Set_15\\IndTrain_equVal"
 	std::string folder = typeid(learner).name();
 	folder.erase(0, 6).append("\\IndTrain_test");
 	namedir.append(folder);
 	cout << namedir << endl;
 	string IndFile = create_filename(namedir, learner, seed, bias);
 	indOutput.open(IndFile.c_str());
-	indOutput << "Training" << '\t' << "Age" << '\t' << "Alpha" << '\t' << "Gamma" << '\t' << "Tau" << '\t' << "Bias" << '\t' << "Current.Reward" << '\t' << "Cum.Reward" << '\t' << "Neg.Reward" << '\t';
-	indOutput << "value_choice" << '\t' << "Type_choice" << '\t' << "Height_choice" << '\t' << "Length_choice" << '\t' << "redMain_choice" << '\t' << "greenMain_choice" << '\t' << "blueMain_choice" << '\t';
-	indOutput << "redSec_choice" << '\t' << "greenSec_choice" << '\t' << "blueSec_choice" << '\t' << "secCol_choice" << '\t' << "strip_choice" << '\t' << "dots_choice" << '\t';
-	indOutput << "value_discard" << '\t' << "Type_discard" << '\t' << "Height_discard" << '\t' << "Length_discard" << '\t' << "redMain_discard" << '\t' << "greenMain_discard" << '\t' << "blueMain_discard" << '\t';
-	indOutput << "redSec_discard" << '\t' << "greenSec_discard" << '\t' << "blueSec_discard" << '\t' << "secCol_discard" << '\t' << "strip_discard" << '\t' << "dots_discard" << '\t';
-	indOutput << "DPUpdate" << '\t';
+	indOutput << "Training" << '\t' << "Age" << '\t' << "Alpha" << '\t';
+	indOutput << "Gamma" << '\t' << "Tau" << '\t' << "Neta" << '\t';
+	indOutput << "Outbr" << '\t' << "Current.Reward" << '\t' << "Cum.Reward";
+	indOutput << '\t' << "Neg.Reward" << '\t';
+	indOutput << "value_choice" << '\t' << "Type_choice" << '\t' << "Height_choice" << '\t';
+	indOutput << "Length_choice" << '\t' << "redMain_choice" << '\t' << "greenMain_choice" << '\t';
+	indOutput << "blueMain_choice" << '\t' << "redSec_choice" << '\t' << "greenSec_choice" << '\t';
+	indOutput << "blueSec_choice" << '\t' << "secCol_choice" << '\t' << "strip_choice" << '\t';
+	indOutput << "dots_choice" << '\t' << "value_discard" << '\t' << "Type_discard" << '\t';
+	indOutput << "Height_discard" << '\t' << "Length_discard" << '\t' << "redMain_discard" << '\t';
+	indOutput << "greenMain_discard" << '\t' << "blueMain_discard" << '\t' << "redSec_discard" << '\t';
+	indOutput << "greenSec_discard" << '\t' << "blueSec_discard" << '\t' << "secCol_discard" << '\t';
+	indOutput << "strip_discard" << '\t' << "dots_discard" << '\t';
 	if (learner.numEst > 11)
 	{
 		if (learner.numEst > 23)
 		{
-			indOutput << "Height_1_0" << '\t' << "Length_1_0" << '\t' << "redMain_1_0" << '\t' << "greenMain_1_0" << '\t' << "blueMain_1_0" << '\t';
-			indOutput << "redSec_1_0" << '\t' << "greenSec_1_0" << '\t' << "blueSec_1_0" << '\t' << "secCol_1_0" << '\t' << "strip_1_0" << '\t' << "dots_1_0" << '\t';
-			indOutput << "Height_2_0" << '\t' << "Length_2_0" << '\t' << "redMain_2_0" << '\t' << "greenMain_2_0" << '\t' << "blueMain_2_0" << '\t';
-			indOutput << "redSec_2_0" << '\t' << "greenSec_2_0" << '\t' << "blueSec_2_0" << '\t' << "secCol_2_0" << '\t' << "strip_2_0" << '\t' << "dots_2_0" << '\t';
-			indOutput << "Height_1_1" << '\t' << "Length_1_1" << '\t' << "redMain_1_1" << '\t' << "greenMain_1_1" << '\t' << "blueMain_1_1" << '\t';
-			indOutput << "redSec_1_1" << '\t' << "greenSec_1_1" << '\t' << "blueSec_1_1" << '\t' << "secCol_1_1" << '\t' << "strip_1_1" << '\t' << "dots_1_1" << '\t';
-			indOutput << "Height_2_1" << '\t' << "Length_2_1" << '\t' << "redMain_2_1" << '\t' << "greenMain_2_1" << '\t' << "blueMain_2_1" << '\t';
-			indOutput << "redSec_2_1" << '\t' << "greenSec_2_1" << '\t' << "blueSec_2_1" << '\t' << "secCol_2_1" << '\t' << "strip_2_1" << '\t' << "dots_2_1" << '\t';
+			indOutput << "Height_1_0" << '\t' << "Length_1_0" << '\t' << "redMain_1_0" << '\t';
+			indOutput << "greenMain_1_0" << '\t' << "blueMain_1_0" << '\t' << "redSec_1_0" << '\t';
+			indOutput << "greenSec_1_0" << '\t' << "blueSec_1_0" << '\t' << "secCol_1_0" << '\t';
+			indOutput << "strip_1_0" << '\t' << "dots_1_0" << '\t' << "Height_2_0" << '\t';
+			indOutput << "Length_2_0" << '\t' << "redMain_2_0" << '\t' << "greenMain_2_0" << '\t';
+			indOutput << "blueMain_2_0" << '\t' << "redSec_2_0" << '\t' << "greenSec_2_0" << '\t';
+			indOutput << "blueSec_2_0" << '\t' << "secCol_2_0" << '\t' << "strip_2_0" << '\t';
+			indOutput << "dots_2_0" << '\t' << "Height_1_1" << '\t' << "Length_1_1" << '\t' ;
+			indOutput << "redMain_1_1" << '\t' << "greenMain_1_1" << '\t' << "blueMain_1_1" << '\t';
+			indOutput << "redSec_1_1" << '\t' << "greenSec_1_1" << '\t' << "blueSec_1_1" << '\t';
+			indOutput << "secCol_1_1" << '\t' << "strip_1_1" << '\t' << "dots_1_1" << '\t';
+			indOutput << "Height_2_1" << '\t' << "Length_2_1" << '\t' << "redMain_2_1" << '\t';
+			indOutput << "greenMain_2_1" << '\t' << "blueMain_2_1" << '\t';
+			indOutput << "redSec_2_1" << '\t' << "greenSec_2_1" << '\t' << "blueSec_2_1" << '\t';
+			indOutput << "secCol_2_1" << '\t' << "strip_2_1" << '\t' << "dots_2_1" << '\t';
 		}
 		else
 		{
-			indOutput << "Height_1" << '\t' << "Length_1" << '\t' << "redMain_1" << '\t' << "greenMain_1" << '\t' << "blueMain_1" << '\t';
-			indOutput << "redSec_1" << '\t' << "greenSec_1" << '\t' << "blueSec_1" << '\t' << "secCol_1" << '\t' << "strip_1" << '\t' << "dots_1" << '\t';
-			indOutput << "Height_2" << '\t' << "Length_2" << '\t' << "redMain_2" << '\t' << "greenMain_2" << '\t' << "blueMain_2" << '\t';
-			indOutput << "redSec_2" << '\t' << "greenSec_2" << '\t' << "blueSec_2" << '\t' << "secCol_2" << '\t' << "strip_2" << '\t' << "dots_2" << '\t';
-			indOutput << "featChoice";
+			indOutput << "Height_1" << '\t' << "Length_1" << '\t' << "redMain_1" << '\t';
+			indOutput << "greenMain_1" << '\t' << "blueMain_1" << '\t';
+			indOutput << "redSec_1" << '\t' << "greenSec_1" << '\t' << "blueSec_1" << '\t';
+			indOutput << "secCol_1" << '\t' << "strip_1" << '\t' << "dots_1" << '\t';
+			indOutput << "Height_2" << '\t' << "Length_2" << '\t' << "redMain_2" << '\t';
+			indOutput << "greenMain_2" << '\t' << "blueMain_2" << '\t' << "redSec_2" << '\t';
+			indOutput << "greenSec_2" << '\t' << "blueSec_2" << '\t' << "secCol_2" << '\t';
+			indOutput << "strip_2" << '\t' << "dots_2" << '\t' << "featChoice";
 		}
 	}
 	else
 	{
-		indOutput << "Height_1" << '\t' << "Length_1" << '\t' << "redMain_1" << '\t' << "greenMain_1" << '\t' << "blueMain_1" << '\t';
-		indOutput << "redSec_1" << '\t' << "greenSec_1" << '\t' << "blueSec_1" << '\t' << "secCol_1" << '\t' << "strip_1" << '\t' << "dots_1" << '\t';
+		indOutput << "Height_1" << '\t' << "Length_1" << '\t' << "redMain_1" << '\t';
+		indOutput << "greenMain_1" << '\t' << "blueMain_1" << '\t' << "redSec_1" << '\t';
+		indOutput << "greenSec_1" << '\t' << "blueSec_1" << '\t' << "secCol_1" << '\t';
+		indOutput << "strip_1" << '\t' << "dots_1" << '\t';
 	}
 	indOutput << endl;
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	std::ifstream input("D:\\quinonesa\\learning_models_c++\\functionAprox\\test.json");
+	if (!input.is_open()) { cout << "JSON file is not open" << endl; }
+	nlohmann::json param = nlohmann::json::parse(input);
+
+	cout << param.dump() << endl;
+
+	int const totRounds = param["totRounds"];
+	double ResReward = param["ResReward"];
+	double VisReward = ResReward;
+	double ResProb = param["ResProb"];
+	double VisProb = ResProb;
+	double ResProbLeav = param["ResProbLeav"];
+	double VisProbLeav = param["VisProbLeav"];
+	double negativeRew = param["negativeRew"];
+	bool experiment = param["experiment"];
+	double inbr = param["inbr"];
+	double outbr = param["outbr"];
+	int trainingRep = param["trainingRep"];
+	double alphaT = param["alphaT"];
+	const int numlearn = 2;
+	int printGen = param["printGen"];
+	int seed = param["seed"];
+
+	double gammaT;
+
+	double gammaRange[3] = { param["gammaRange"] };
+
+	double tauT;
+
+	double tauRange[4] = { param["tauRange"] };
+
+	double netaT;
+
+	double netaRange[2] = { param["netaRange"] };
+
 	for (size_t i = 0; i < 8; i++)
 	{
 		visitMeans[i] = 40;
@@ -167,36 +235,37 @@ int _tmain(int argc, _TCHAR* argv[])
 	visitMeans[1] = 20, residMeans[1] = 30;
 	residProbs[0] = 0;
 	mins[0] = 10, mins[1] = 10;
-	int totRounds = 100000;
-	ResReward = 10;
-	VisReward = 10;
-	double ResProb = 0.2;
-	double VisProb = 0.2;
-	double ResProbLeav = 0;
-	double VisProbLeav = 1;
-	double negativeRew = -10;
-	bool experiment = 0;
-	double inbr = 0.0;
-	double outbr = 0;
-	int const trainingRep = 15;//30
-	double alphaT = 0.000001;
-	const int numlearn = 2;
-	int printGen = 10;
+	//int totRounds = 100000;
+	//ResReward = 10;
+	//VisReward = 10;
+	//double ResProb = 0.2;
+	//double VisProb = 0.2;
+	//double ResProbLeav = 0;
+	//double VisProbLeav = 1;
+	//double negativeRew = -10;
+	//bool experiment = 0;
+	//double inbr = 0.0;
+	//double outbr = 0;
+	//int const trainingRep = 15;//30
+	//double alphaT = 0.000001;
+	//const int numlearn = 2;
+	//int printGen = 10;
 
-	double gammaT;
+	//double gammaT;
 
-	double gammaRange[3] = { 0, 0.5, 0.8 };
+	//double gammaRange[3] = { 0, 0.5, 0.8 };
 
-	double tauT;
+	//double tauT;
 
-	double tauRange[3] = { 1, 2, 5};
-	
-	double netaT = 0;
+	//double tauRange[3] = { 1, 2, 5};
+	//
+	//double netaT = 0;
 
-	double netaRange[1] = { 0.5 };
+	//double netaRange[1] = { 0.5 };
 
 
-	int seed = 12;
+	//int seed = 12;
+
 	rnd::set_seed(seed);
 
 	client *clientSet;
@@ -205,45 +274,48 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	agent *learners[numlearn];
 
-
-	for (size_t k = 0; k < 3; k++)
+	for (int n = 0; n < 2; n++)
 	{
-		for (size_t l = 0; l < 3; l++)
+		for (size_t k = 0; k < 3; k++)
 		{
-			gammaT = gammaRange[k];
-			tauT = tauRange[0];
-			ofstream printTest;
-			ofstream DPprint;
-			
-			learners[0] = new StatPosTyp1(alphaT, gammaT, tauT, netaT);
-			learners[4] = new ActPosTy1(alphaT, gammaT, tauT, netaT);
-			
-			//learners[0] = new StatPosTyp2(alphaT, gammaT, tauT);
-			
-			for (int k=0;k<1;++k)  //numlearn
+			for (size_t l = 0; l < 3; l++)
 			{
-				initializeIndFile(printTest, *learners[k], seed, outbr);
-				for (int i = 0; i < trainingRep; i++)
+				netaT = netaRange[n];
+				gammaT = gammaRange[k];
+				tauT = tauRange[0];
+				ofstream printTest;
+				ofstream DPprint;
+
+				learners[0] = new StatPosTyp1(alphaT, gammaT, tauT, netaT);
+				learners[4] = new ActPosTy1(alphaT, gammaT, tauT, netaT);
+
+				//learners[0] = new StatPosTyp2(alphaT, gammaT, tauT);
+
+				for (int k = 0; k < 2; ++k)  //numlearn
 				{
-					draw(clientSet, totRounds, ResProb, VisProb);
-					idClientSet = 0;
-					for (int j = 0; j < totRounds; j++)
+					initializeIndFile(printTest, *learners[k], seed, outbr);
+					for (int i = 0; i < trainingRep; i++)
 					{
-						learners[k]->act(clientSet, idClientSet, VisProbLeav, ResProbLeav, VisReward, ResReward,inbr,outbr, negativeRew,experiment);
-						learners[k]->updateDerived();
-						if (j%printGen == 0)
+						draw(clientSet, totRounds, ResProb, VisProb);
+						idClientSet = 0;
+						for (int j = 0; j < totRounds; j++)
 						{
-							learners[k]->printIndData(printTest, i, outbr);
+							learners[k]->act(clientSet, idClientSet, VisProbLeav, ResProbLeav, VisReward, ResReward, inbr, outbr, negativeRew, experiment);
+							learners[k]->updateDerived();
+							if (j%printGen == 0)
+							{
+								learners[k]->printIndData(printTest, i, outbr);
+							}
 						}
+						learners[k]->rebirth();
 					}
-					learners[k]->rebirth();
+					printTest.close();
+					learners[k]->DPupdate(ResProb, VisProb, VisProbLeav, ResProbLeav, outbr, ResReward, VisReward, negativeRew, DPprint, experiment);
+					DPprint.close();
+					delete learners[k];
 				}
-				printTest.close();
-				learners[k]->DPupdate(ResProb, VisProb, VisProbLeav, ResProbLeav, outbr, ResReward, VisReward,negativeRew, DPprint, experiment);
-				DPprint.close();
-				delete learners[k];
+
 			}
-			
 		}
 	}
 
