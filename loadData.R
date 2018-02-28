ef196de6f9d05739e88dd6bb65d701c53487f053
QUINONES PARADES Andrès <andres.quinones@unine.ch>
2018-02-28 13:21:48 +0100

    Get ready for merge

--- loadData.R
+++ loadData.R
@@ -1,5 +1,7 @@
 # ------------------------ Load data ------------------------------------------#
 
+library("data.table")
+
 genDir<-"D:\\quinonesa\\Simulation\\functionAprox\\"
 
 agenDir<-paste(genDir,"ActPosTy1",sep='')
@@ -8,8 +10,19 @@ setwd(agenDir)
 
 listAgen<-list.files()
 
+listTrain<-grep('IndTrain',listAgen,value = TRUE)
+
+rawdata<-do.call(rbind,lapply(listTrain, fread))
+
 filtList<-function(list,alpha,gamma,tau,neta,outbr)
 {
   gamma<-c(0,0.5)
   gammaList<-lapply(gamma, paste,"gamma",sep="")
 }
+
+with(rawdata[(Gamma==0.8 & Tau==10) & Training==1],{
+  plot(x=Age,y=Height_1)  
+})
+
+
+
